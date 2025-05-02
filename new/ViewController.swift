8b9d6ed869628c8f0aac7e646353ccce9ed7d3f5
Celio Junior <cxliobj@outlook.com>
2025-05-02 00:38:30 -0300

    Static app working

--- new/ViewController.swift
+++ new/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
+enum Shift: String
+{
+    case morning = "Good Morning"
+    case night = "Good Night"
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
+    var name: String = ""
+    var shift: Shift = .morning
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setLayout()
+        label.text = "OIE"
+    }
+    
+    func setLayout()
+    {
+        view.backgroundColor = UIColor(named: (shift == .morning) ? "Orange" : "Purple")
+        
+        label.text = "\(shift.rawValue) \(name)"
+        label.textColor = UIColor(named: (shift != .morning) ? "Orange" : "Purple")
+        image.image = UIImage(named: (shift == .morning) ? "GoodMorning" : "GoodNight")
     }
 
-
+    @IBAction func changeShift(_ sender: Any) {
+        self.shift = (shift == .morning) ? .night : .morning
+        setLayout()
+    }
+    @IBAction func changeName(_ sender: Any) {
+        self.name = textField.text ?? ""
+        textField.text = ""
+        setLayout()
+    }
+    
 }
 
